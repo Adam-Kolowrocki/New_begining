6d3c4e387a9ee6b244cfd9c5486ef864622319f7
Adam Kołowrocki <adamkolowrocki@gmail.com>
2022-09-29 20:54:38 +0200

    Update README.md

---
+++ 02_variables_&_types/07_stringi_07.py
@@ -0,0 +1,6 @@
+# Stwórz dwie dowolne zmienne przechowujące wartość liczbową i tekstową.
+# Użyj funkcji format(), by wyświetlić zdanie zawierające te wartości.
+var_num = 549
+var_alp = "Konstantynopol"
+print(f'I would like to {var_num} test "format" {var_alp} funktion.')
+print('Second test {} of "format" {} function.'.format(var_num, var_alp))
