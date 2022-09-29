a59749a0e0e24a14db250724c9351cd025d51702
Adam Kołowrocki <adamkolowrocki@gmail.com>
2022-09-29 21:02:14 +0200

    Update README.md

--- 02_variables_&_types/07_stringi_07.py
+++ 02_variables_&_types/07_stringi_07.py
@@ -1,6 +1,9 @@
-# Stwórz dwie dowolne zmienne przechowujące wartość liczbową i tekstową.
-# Użyj funkcji format(), by wyświetlić zdanie zawierające te wartości.
 var_num = 549
 var_alp = "Konstantynopol"
 print(f'I would like to {var_num} test "format" {var_alp} funktion.')
 print('Second test {} of "format" {} function.'.format(var_num, var_alp))
+var_1 = 'France'
+var_2 = 'Belgium'
+var_3 = 'Germany'
+print(f'I would like to go for a trip to {var_1}. But first would have to cross {var_3} and again visit {var_2}.')
+print("This holiday I'm going to {} and maby {} as well. At the road back I will see {}.".format(var_2, var_1, var_3))
