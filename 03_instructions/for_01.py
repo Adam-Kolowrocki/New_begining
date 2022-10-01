82efaba4b5081fc3f987657c28a175df70d75c9f
Adam Kołowrocki <adamkolowrocki@gmail.com>
2022-10-01 11:29:53 +0200

    Update README.md

---
+++ 03_instructions/for_01.py
@@ -0,0 +1,8 @@
+# Create a list o items You take with You for lonely mountain trip.
+# Print name of just packet item, after the last one print:
+# “Great, we are ready!”
+items_list = ['backpack', 'chocolate', 'jacket', 'buts', 'hot drink', 'sleeping bag', 'tent', 'knife', 'warm socks']
+print('For a mountain tripp I would take: ')
+for item in items_list:
+    print(item)
+print('Great, we are ready!')
---
+++ 03_instructions/for_02.py
@@ -0,0 +1,4 @@
+# Create a list of ingredients for Your favorite dish.
+# Print, what You should add in order.
+# Outside the loop add additional instructions, eg. “Into the oven”, “Serve cooled”.
+
