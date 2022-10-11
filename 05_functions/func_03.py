11b8731107902b87a0aa5c6d2d8b42a69a11e3b1
Adam Kołowrocki <adamkolowrocki@gmail.com>
2022-10-11 22:11:27 +0200

    Update README.md

--- 05_functions/func_03.py
+++ 05_functions/func_03.py
@@ -1,2 +1,17 @@
 # Write a function which take a list o numbers and returns sum of all its elements.
-def list_sum(giv_list):
+def list_sum(n_list):
+    """This function returns a sum of elements in the given list."""
+    sum = 0
+    for n in n_list:
+        sum += n
+    return sum
+
+
+user_list = []
+print(f'This code returns sum of elements in list given by user.\n')
+elem_count = int(input(f'Hov many elements do You want to add to list -> '))
+for n in range(1, elem_count + 1):
+    user_ele = int(input(f'Type {n} number -> '))
+    user_list.append(user_ele)
+
+print(list_sum(user_list))
---
+++ 05_functions/func_04.py
@@ -0,0 +1,18 @@
+# Write a function which returns every even elements from given list of elements (use function from task 2)
+def if_even(user_list):
+    """This function check if numbers given in list are even."""
+    result_list = []
+    for n in user_list:
+        if n % 2 == 0:
+            result_list.append(n)
+    return result_list
+
+
+user_list = []
+print(f'This code returns even elements of list given by user.\n')
+elem_count = int(input(f'Hov many elements do You want to add to list -> '))
+for n in range(1, elem_count + 1):
+    user_ele = int(input(f'Type {n} number -> '))
+    user_list.append(user_ele)
+
+print(if_even(user_list))
