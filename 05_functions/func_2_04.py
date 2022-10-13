28aa3cd34365d1262c381f00a9b13b5a82596c5b
Adam Kołowrocki <adamkolowrocki@gmail.com>
2022-10-14 00:41:29 +0200

    Update README.md

--- 05_functions/func_2_04.py
+++ 05_functions/func_2_04.py
@@ -3,20 +3,24 @@
 
 def usr_range():
     low_end = int(input(f'What is the low end of the range -> '))
-    high_end = int(input(f'What is the high end of the range -> '))
+    high_end: int = int(input(f'What is the high end of the range -> '))
+    while low_end > high_end:
+        print(f'You gave wrong range numbers.\n')
+        low_end = int(input(f'What is the low end of the range -> '))
+        high_end: int = int(input(f'What is the high end of the range -> '))
+        continue
     return low_end, high_end
 
 
 def range_check(low, high):
     x = 18
-    if low > high:
-        print(f'You gave wrong range numbers.')
-        usr_range()
-    elif low <= x <= high:
-        print(f'Yes, number {x} is in the given range.')
+    if low <= x <= high:
+        print(f'Yes, number {x} is in the given range. X = {x}')
     else:
         print(f'No, the number X is out of given range.')
 
 
 print(f'This code checks if the constant number "x" is in the range given by user.')
-range_check(low=usr_range()[0], high=usr_range()[1])
+low, high = usr_range()
+
+range_check(low, high)
