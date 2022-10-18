977cad7a474fc3d6f3601443e9592c1ced22c1f9
Adam Kołowrocki <adamkolowrocki@gmail.com>
2022-10-18 22:40:21 +0200

    Update README.md

--- 06_Christmas_tree/RPS_03.py
+++ 06_Christmas_tree/RPS_03.py
@@ -1,49 +1,72 @@
 # Add option to continue the game regardless of the number of rounds selected.
 # After each round ask user if he wants to continue "Would you like to continue yes/no:"
 # if no, ask again
-# "No!? We had so much fun together! You will not get away so easily!
-# Prepare yourself for the next round!"
+# "No!? We had so much fun together! You will not get away so easily! Prepare yourself for the next round!"
 # And start a round. Add menu with options and welcome screen.
-# Welcome:                      Level selection:
-# S - Start                     Difficulty level:
-# P - Difficulty level          A - Smart
-# Q - Quit                      B - Stupid
-#
-# Standardize communicates given to user:
-# Your choice:
-# R - Rock
-# P - Paper
-# S - Scissors
-#
+# Welcome:                      Level selection:        Standardize communicates given to user:
+# S - Start                     Difficulty level:       Your choice:
+# P - Difficulty level          A - Smart               R - Rock
+# Q - Quit                      B - Stupid              P - Paper
+#                                                       S - Scissors
 # End of game:
 # Would you like to continue yes/no:
 #
 # You don't know how to extend difficulty level ?
-# Lizard and Spock
-# Like in  Big Bang Theory
+# Lizard and Spock, Like in  Big Bang Theory...
 # Add statistics :
 # Till user break the game, collect statistics and at the end print:
 # Count and percent od User wins
 # The Longest sequence of user and computer wins
 # Others - version 04
 from random import choice
+round_counter = 0
+draw_count = 0
+user_wins = 0
+comp_wins = 0
+
+
+def main():
+    menu()
 
 
 def menu():
-    """Menu and info function"""
-    print(f'Lets play a game - Rock/Paper/Scissors')
+    """Main menu and info function"""
+    print(f'Lets play a game - Rock/Paper/Scissors\n')
+    print(f'You have few options:\nS - Start\nD - Difficulty level\nQ - Quit\n')
+    print(f'\nAnytime during the game You can end the game just by typing "q".')
+    user_choice = ""
+    choice_options = ["s", "d", "q"]
+    while user_choice.lower() not in choice_options:
+        user_choice = input(f'What is Your Choice -> ')
+    if user_choice == "q":
+        game_over()
+    elif user_choice == "s":
+        round_count()
+    elif user_choice == "d":
+        game_difficulty()
+
+
+def game_over():
+    print(f'Game Over')
+
+
+def game_difficulty():
+    print(f'You have a choice of difficulty.\nA - Smart\nB - Stupid')
+    game_diff = ""
+    choice_options = ["a", "b"]
+    while game_diff.lower() not in choice_options:
+        game_diff = input(f'What is Your choice -> ')
+    round_count(game_diff.lower())
+
+
+def round_count(diff):
     print(f'\nYou can chose how many rounds You want to play...')
-    user_number = int(input('Type a number of rounds -> '))
-    print(f'\nAnytime You want, You can end the game just by typing "end".')
-    return user_number
+    round_number = int(input('Type a number of rounds -> '))
+    return diff, round_number
 
 
-def main_game(user_number):
+def a_game(user_number):
     """Function compare user choose with computer choose"""
-    round_counter = 0
-    draw_count = 0
-    user_wins = 0
-    comp_wins = 0
     while round_counter <= user_number - 1:
         options = 'r', 'p', 's'
         print(f'\nYou can chose one of "r" as for rock, "p" as for paper or "s" as for scissors.')
@@ -88,16 +111,17 @@ def main_game(user_number):
 
 def game_end(user_wins, comp_wins, draw_count):
     """This function prints result and info """
-    print(f'\nThere was {user_wins} round won by You, {comp_wins} rounds won by computer and {draw_count} draws.')
-    if user_wins == comp_wins:
-        print(f'\nSo there was a draw in a whole game.')
-    elif user_wins > comp_wins:
-        print(f'\nSo You won the whole game.\n\nCongratulations!!!')
-    elif user_wins < comp_wins:
-        print(f'\nSo the computer won whole game.\n\nSorry!!!')
+    if user_wins == comp_wins == draw_count == 0:
+        print(f'Game Over')
+    else:
+        print(f'\nThere was {user_wins} round won by You, {comp_wins} rounds won by computer and {draw_count} draws.')
+        if user_wins == comp_wins:
+            print(f'\nSo there was a draw in a whole game.')
+        elif user_wins > comp_wins:
+            print(f'\nSo You won the whole game.\n\nCongratulations!!!')
+        elif user_wins < comp_wins:
+            print(f'\nSo the computer won whole game.\n\nSorry!!!')
 
-
-game_end(*main_game(menu()))
 
 if __name__ == "__main__":
     main()
