90c7eb3bbd9f5b6f57495da5679b09682928156c
Adam Kołowrocki <adamkolowrocki@gmail.com>
2022-11-07 21:59:17 +0100

    Update README.md

--- 06_Christmas_tree/TTT_Tests.py
+++ 06_Christmas_tree/TTT_Tests.py
@@ -1,34 +1,20 @@
-table_2 = [
-    ['  1   2   3'],
-    ['A . | . | .'],
-    ['B . | . | .'],
-    ['C . | . | .']
-]
-for col in table_2:
-    for row in col:
-        print(row, '\n', end='')
-
 table_3 = [
     [' ', '1', ' ', '2', ' ', '3'],
     ['A', '.', '|', '.', '|', '.'],
     ['B', '.', '|', '.', '|', '.'],
     ['C', '.', '|', '.', '|', '.'],
 ]
-# print(table_3[0])
-# print(table_3[0][1])
-# print(type(table_3[0][1]))
-for col in table_3:
-    i = 0
-    while i in range(len(col)):
-        print(col[i])
-        i += 1
-# for row in col:
-# print(row)
-
-# table_2[1][0].replace('A', 'X', 1)
-# table_2[3][0].replace('.', 'O', 1)
-# for row in table_2:
-#       for col in row:
-#             print(col, '\n', end='')
-#
-# print(type(table_2[2][0]))
+for i in range(len(table_3)):
+    for j in range(len(table_3[i])):
+        print(table_3[i][j], end=' ')
+    print()
+table_3[1][1] = 'X'
+for i in range(len(table_3)):
+    for j in range(len(table_3[i])):
+        print(table_3[i][j], end=' ')
+    print()
+table_3[2][3] = 'O'
+for i in range(len(table_3)):
+    for j in range(len(table_3[i])):
+        print(table_3[i][j], end=' ')
+    print()
--- 06_Christmas_tree/tic-tac-toe_01.py
+++ 06_Christmas_tree/tic-tac-toe_01.py
@@ -36,17 +36,19 @@
 
 # Advice - do not start from display. Empty fixed table is not a problem.
 # Plan the play creating algorithm schema of necessary steps.
+table = [
+        [' ', '1', ' ', '2', ' ', '3'],
+        ['A', '.', '|', '.', '|', '.'],
+        ['B', '.', '|', '.', '|', '.'],
+        ['C', '.', '|', '.', '|', '.'],
+    ]
 
 
 def main():
     """Main function of the game"""
     print(f'Lets play "Tic Tac Toe" game.\n')
-    print('   1   2   3\n'
-          'A  . | . | .\n'
-          'B  . | . | .\n'
-          'C  . | . | .\n')
-
-    print(f'It is not needed to use capital letters.')
+    table_print(table, '0', '0')
+    print(f'\nIt is not needed to use capital letters.')
     input(f'Press any key to continue...')
 
 
@@ -55,12 +57,67 @@ def player_names(player):
     return player_name
 
 
-def table_print(move, sign):
-    # move = a1
-    print('   1   2   3\n'
-          'A  . | . | .\n'
-          'B  . | . | .\n'
-          'C  . | . | .\n')
+def table_print(table, move, sign):
+    if move[0] == 'a' and move[1] == '1':
+        if table[1][1] == '.':
+            table[1][1] = sign
+        else:
+            print(f'This move is not allowed...')
+            # get_move(sign)
+    elif move[0] == 'a' and move[1] == '2':
+        if table[1][3] == '.':
+            table[1][3] = sign
+        else:
+            print(f'This move is not allowed...')
+            get_move(sign)
+    elif move[0] == 'a' and move[1] == '3':
+        if table[1][5] == '.':
+            table[1][5] = sign
+        else:
+            print(f'This move is not allowed...')
+            get_move(sign)
+    elif move[0] == 'b' and move[1] == '1':
+        if table[2][1] == '.':
+            table[2][1] = sign
+        else:
+            print(f'This move is not allowed...')
+            get_move(sign)
+    elif move[0] == 'b' and move[1] == '2':
+        if table[2][3] == '.':
+            table[2][3] = sign
+        else:
+            print(f'This move is not allowed...')
+            get_move(sign)
+    elif move[0] == 'b' and move[1] == '3':
+        if table[2][5] == '.':
+            table[2][5] = sign
+        else:
+            print(f'This move is not allowed...')
+            get_move(sign)
+    elif move[0] == 'c' and move[1] == '1':
+        if table[3][1] == '.':
+            table[3][1] = sign
+        else:
+            print(f'This move is not allowed...')
+            get_move(sign)
+    elif move[0] == 'c' and move[1] == '2':
+        if table[3][3] == '.':
+            table[3][3] = sign
+        else:
+            print(f'This move is not allowed...')
+            get_move(sign)
+    elif move[0] == 'c' and move[1] == '3':
+        if table[3][5] == '.':
+            table[3][5] = sign
+        else:
+            print(f'This move is not allowed...')
+            get_move(sign)
+
+    for i in range(len(table)):
+        for j in range(len(table[i])):
+            print(table[i][j], end=' ')
+        print()
+    return table
 
 
 moves = ['a1', 'a2', 'a3', 'b1', 'b2', 'b3', 'c1', 'c2', 'c3']
@@ -77,10 +134,12 @@ def get_move(player):
 if __name__ == "__main__":
     main()
 
+
 player_x = player_names('Player X')
 player_o = player_names('Player O')
-while True:
+
+while '.' in table[1] or '.' in table[2] or '.' in table[3]:
     player_x_move = get_move(player_x)
-    table_print(player_x_move, 'X')
+    table_print(table, player_x_move, 'X')
     player_o_move = get_move(player_o)
-    table_print(player_o_move, 'O')
+    table_print(table, player_o_move, 'O')
