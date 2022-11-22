28a24f6ad5fdf9df5354c2d7a0c2e2afd9003096
Adam Kołowrocki <adamkolowrocki@gmail.com>
2022-11-22 20:24:43 +0100

    Update README.md

---
+++ 07_File_operations/files_07_hangman.py
@@ -0,0 +1,9 @@
+# Hangman. Make a file containing list of words in categories, e.g. animals, fruits, etc.
+# Ask user to choose category before play. Next read list of words from chosen file, chose random one.
+# The play should be as much intuitive as possible.
+def main():
+    print(f'Hangman')
+
+
+if __name__ == "__main__":
+    main()
