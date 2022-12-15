e8732557b8e7dc7ea756c28c9b3efeab2c86d270
Adam-Kolowrocki <adamkolowrocki@gmail.com>
2022-12-15 13:04:12 +0100

    Update README.md

--- 07_File_operations/file_converter.py
+++ 07_File_operations/file_converter.py
@@ -1,28 +1,26 @@
-# Create a generator of any type, for e.g. generator of sentences, tweets or conferences.
-# Input data from csv file, (csv file can be threatened as txt with known separator), which will contain data
-# needed to generate.
+# File converter for Quota generator
 with open('words_base.txt') as input_file:
     input_data = input_file.readlines()
 for i in range(len(input_data)):
-    input_data[i] = input_data[i].replace('        ', '')
-    input_data[i] = input_data[i].replace('"', '')
-    input_data[i] = input_data[i].replace(' ,\n', '')
-print(input_data)
+    input_data[i] = input_data[i].replace('        ', '').replace('"', '').replace(' ,\n', '').replace(',\n', '')
 
 first_stop = input_data.index('\n')
 second_stop = input_data.index('\n', first_stop + 3)
 third_stop = input_data.index('\n', second_stop + 3)
-starts = input_data[0: first_stop]
-middles = input_data[first_stop + 3: second_stop]
-qualifiers = input_data[second_stop + 3: third_stop]
-finishes = input_data[third_stop + 3:]
-for i in range(len(finishes)):
-    finishes[i] = finishes[i].replace(',\n', '')
-print(f'{len(starts)} = starts = {starts}')
-print(f'{len(middles)} = middles = {middles}')
-print(f'{len(qualifiers)} = qualifiers {qualifiers}')
-print(f'{len(finishes)} = finishes = {finishes}')
+starts, middles, qualifiers, finishes = input_data[0: first_stop], input_data[first_stop + 3: second_stop], \
+                                        input_data[second_stop + 3: third_stop], input_data[third_stop + 3:]
+
+
 with open('data3.csv', '+w') as new_data_csv:
-    for i in range(len(starts)):
+    for i in range(len(finishes)):
         tmp_str = starts[i] + ',' + middles[i] + ',' + qualifiers[i] + ',' + finishes[i] + '\n'
+        new_data_csv.write(tmp_str)
+    for i in range(len(finishes), len(middles)):
+        tmp_str = starts[i] + ',' + middles[i] + ',' + qualifiers[i] + '\n'
+        new_data_csv.write(tmp_str)
+    for i in range(len(middles), len(qualifiers)):
+        tmp_str = starts[i] + ',' + qualifiers[i] + '\n'
+        new_data_csv.write(tmp_str)
+    for i in range(len(qualifiers), len(starts)):
+        tmp_str = starts[i] + '\n'
         new_data_csv.write(tmp_str)
--- 07_File_operations/files_08_generator_1.py
+++ 07_File_operations/files_08_generator_1.py
@@ -1,8 +1,61 @@
 # Create a generator of any type, for e.g. generator of sentences, tweets or conferences.
 # Input data from csv file, (csv file can be threatened as txt with known separator), which will contain data
 # needed to generate.
-import json
+import csv
+from random import choice
 
-with open('./default_deepak_data.json') as json_file:
-    data = json.load(json_file)
-    print(data)
+
+def main():
+    """Main function - just print info"""
+    print(f'This is a generator...'.center(100))
+    print()
+    print(f'It generates random quotes...'.center(100))
+    print()
+    input(f'Press [ENTER]')
+    generator(*data_import())
+
+
+# def data_import():
+#     starts = [], middles, qualifiers, finishes = [], [], [], []
+#     with open('./data3.csv') as file:
+#         for i, line in enumerate(file):
+#             first_coma = line.find(',')
+#             second_coma = line.find(',', first_coma + 1, )
+#             third_coma = line.find(',', second_coma + 1, )
+#             starts.append(line[0: first_coma])
+#             middles.append(line[first_coma + 1: second_coma])
+#             qualifiers.append(line[second_coma + 1: third_coma])
+#             finishes.append(line[third_coma + 1:])
+#     return starts, middles, qualifiers, finishes
+def data_import():
+    starts = [], middles, qualifiers, finishes = [], [], [], []
+    with open('./data3.csv') as file:
+        for i, line in enumerate(file):
+            first_coma = line.find(',')
+            starts.append(line[0: first_coma])
+            line = line[first_coma + 1:]
+            second_coma = line.find(',')
+            middles.append(line[0: second_coma])
+            line = line[second_coma + 1:]
+            third_coma = line.find(',')
+            qualifiers.append(line[0: third_coma])
+            line = line[third_coma + 1:]
+            finishes.append(line[third_coma + 1:])
+    return starts, middles, qualifiers, finishes
+
+def generator(starts, middles, qualifiers, finishes):
+    print(len(starts)), print(len(middles)), print(len(qualifiers)), print(len(finishes))
+    print(starts[-3])
+    print(middles[-3])
+    print(qualifiers[-3])
+    print(finishes[-3])
+    print(choice(starts))
+    print(choice(middles))
+    print(choice(qualifiers))
+    print(choice(finishes))
+    quota = choice(starts) + ' ' + choice(middles) + ' ' + choice(qualifiers) + ' ' + choice(finishes)
+    print(f'Generated quota is:\n{quota}')
+
+
+if __name__ == "__main__":
+    main()
