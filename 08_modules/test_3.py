e058e5942ad69394e48f5bafedcc3e8d231aac63
Adam-Kolowrocki <adamkolowrocki@gmail.com>
2022-12-27 21:38:38 +0100

    Update README.md

--- 08_modules/test_3.py
+++
@@ -1,9 +0,0 @@
-import os
-
-filename = 'inwokacja.txt'
-if os.path.isfile(filename):
-    with open(filename, 'r') as f:
-        print(f.read())
-else:
-    print('Nie ma takiego pliku')
-# with open("./Dokumenty/Python-Kurs/New_beginning/0")
