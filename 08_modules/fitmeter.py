e211d929a619578a7a63580211d0f287f949bb5b
Adam-Kolowrocki <adamkolowrocki@gmail.com>
2022-12-28 20:29:15 +0100

    Update README.md

--- 08_modules/fitmeter.py
+++ 08_modules/fitmeter.py
@@ -1,8 +1,30 @@
 from bmi import bmi_calc
 
-print(f'Remember to use dot "." as a separator')
-weight = 82    # input(f'What is Your weight in "kg" >> ')
-height = 1.85    # input('What is Your height in "m" >>' )
 
-print(bmi_calc(weight, height))
+def main():
+    info_return(*bmi_calc(*data_collect()))
 
+
+def data_collect():
+    print(f'Remember to use dot "." as a separator')
+    weight = float(input(f'What is Your weight in "kg" >> '))
+    height = float(input('What is Your height in "m" >> '))
+    return weight, height
+
+
+def info_return(bmi, weight, height):
+    """Return info for the user"""
+    print(f'If You are {height}m tall and weight {weight}kg, Your BMI is: {bmi}')
+    with open('advices.txt', 'r') as f:
+        if bmi <= 18.49:
+            print(f.readlines()[0])
+        elif 18.49 < bmi <= 24.99:
+            print(f.readlines()[1])
+        elif 24.99 < bmi <= 29.99:
+            print(f.readlines()[2])
+        else:
+            print(f.readlines()[3])
+
+
+if __name__ == "__main__":
+    main()
