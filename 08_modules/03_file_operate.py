34ba0edb149e460bf7ddbe310c001d705e945a65
Adam-Kolowrocki <adamkolowrocki@gmail.com>
2022-12-28 23:21:54 +0100

    Update README.md

---
+++ 08_modules/03_file_operate.py
@@ -0,0 +1,24 @@
+# Stwórz moduł, który zajmuje się jedynie otwieraniem plików
+# - oferuje bezpieczny sposób odczytu oraz bezpieczny zapis.
+# Funkcja czytająca pliki sprawdza najpierw czy dany plik istnieje oraz czy jest niepusty.
+# Funkcja zapisująca do pliku chroni przed nadpisaniem istniejącego pliku.
+def main():
+    pass
+
+
+def user_data():
+    filename = input(f'Type the file name with extension -> ')
+    return filename
+
+
+def file_reader(filename):
+
+    pass
+
+
+def file_writer(filename, data):
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ 08_modules/delta.py
@@ -0,0 +1,8 @@
+a = 0
+b = 0
+c = 0
+
+
+def delta(a, b, c):
+    delta = (b * b) - (4 * a * b)
+    return delta
