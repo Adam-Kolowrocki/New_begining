926646a3df99b2fd1046b068ddd7da55d08c33e3
adam <adamkolowrocki@gmail.com>
2023-02-26 10:29:16 +0100

    Update README.md

---
+++ 10_algorithms/tests/turtle_heart.py
@@ -0,0 +1,15 @@
+# Just to draw a heart with turtle
+from turtle import *
+
+pensize(3)
+color("red", "pink")
+begin_fill()
+left(40)
+forward(60)
+circle(30, 200)
+left(240)
+circle(30, 200)
+forward(60)
+end_fill()
+hideturtle()
+done()
---
+++ 10_algorithms/tests/turtle_tests.py
@@ -0,0 +1,18 @@
+import turtle
+
+# turtle.forward(100)
+# turtle.done()
+
+t = turtle.Turtle()
+t.shape('turtle')
+t.forward(100)
+t.left(90)
+t.shape('circle')
+t.right(90)
+t.shape('square')
+t.forward(100)
+t.right(90)
+t.shape('arrow')
+t.forward(100)
+t.forward(100)
+turtle.done()
