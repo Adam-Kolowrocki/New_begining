c31de73187a97e5edbbde6397c8f4ddf31c8ca60
adam <adamkolowrocki@gmail.com>
2023-02-27 19:46:48 +0100

    Update README.md

--- 10_algorithms/recursion/05_alfabetic_sort_rec.py
+++ 10_algorithms/recursion/05_alfabetic_sort_rec.py
@@ -15,19 +15,28 @@
 # Kowal
 # Lipski
 # Wojtczak
+import time
 names_list = []
 with open("05_names.txt", "r") as file:
     names_count = int(file.readline())
     for i in range(names_count):
         names_list.append(file.readline())
+print(f'\nImported list : \n')
+for i in enumerate(names_list):
+    print(i[0] + 1, "->", i[1].replace("\n", ""))
+print(f'\nList sorted alphabetic : \n')
+st = time.time()
 sorted_list = []
 
 
-def sorting_alpa(names):
-    for j in range(len(names)):
-        print(names[j])
-        print(len(names[j]))
-        print(type(names[j]))
+def sorting_alpa(n_list):
+    for j in range(len(n_list)):
+        for k in range(len(n_list[j]) - 1):
+            if n_list[j + 1][k] < n_list[j][k]:
+                tmp = n_list[j + 1]
+                n_list.pop((j + 1))
+                n_list.insert(j, tmp)
+    return n_list
 
 
 sorting_alpa(names_list)
